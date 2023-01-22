5eee60adf4028562de4944488230879751f048aa
Your Name <you@example.com>
2023-01-22 09:41:06 +0200

    ok

--- src/admin_comps/authAdmin.js
+++ src/admin_comps/authAdmin.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { API_URL, doApiGet } from '../services/apiService'
+import { API_URL, doApiGet } from '../services/apiServices'
 import { useNavigate } from "react-router-dom"
 
 export default function AuthAdmin() {
@@ -15,7 +15,7 @@ export default function AuthAdmin() {
       let data = await doApiGet(url);
       if (data.role != "admin") {
         // send to login and show message to login again
-        alert("You need to be admin to login to this area")
+        alert("You need to login again to this area")
         nav("/admin")
       }
     }
