8d644dd9fd63844a7438d28ae9dd7f493eed5d79
Your Name <you@example.com>
2023-02-12 10:38:25 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { ToastContainer } from "react-toastify";
-
-import AddCategory from "./admin_comps/addCategory";
 import AdminHeader from "./admin_comps/adminHeader";
-import AppListAdmin from "./admin_comps/appsListAdmin";
-import CategoriesList from "./admin_comps/categoriesList";
-import EditCategory from "./admin_comps/editCategory";
-import LoginAdmin from "./admin_comps/loginAdmin";
-import UsersList from "./admin_comps/usersList";
-import Home from "./client_comps/home";
 import HeaderClient from "./client_comps/misc/headerClient";
-import PageGamesList from "./client_comps/pageGamesList/pageGamesList";
-import GameInfo from "./client_comps/gameInfo/gameInfo";
-
 import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { adminRoutes, clientRoutes } from "./routesPath/routesPath";
-import CheckIp from "./comps_general/checkIp";
-import { AppContext } from "./context/context";
-import { useContext, useState } from "react";
-import FetchLocation from "./comps_general/checkIp";
 import ForbiddenCountryChecker from "./comps_general/checkIp";
 import { Provider } from "react-redux";
 import {createStore} from 'redux'
@@ -38,7 +23,7 @@ function App() {
             <Route path="/admin/*" element={<AdminHeader />} />
             <Route path="/*" element={<HeaderClient />} />
           </Routes>
-          <ForbiddenCountryChecker>
+          {/*<ForbiddenCountryChecker>*/}
             <ReduxComp/>
             <Routes>
               {/* client */}
@@ -49,7 +34,7 @@ function App() {
               {adminRoutes()}
 
             </Routes>
-          </ForbiddenCountryChecker>
+         {/*</ForbiddenCountryChecker>*/}
           {/* The toast messages added here */}
           <ToastContainer position="top-left" theme="colored" />
         </BrowserRouter>
