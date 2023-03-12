e62a3a98a870b2f6d7ea32befc08d5da3a4c4354
Your Name <you@example.com>
2023-03-12 14:20:27 +0200

    ok

--- src/features/shopSlice.js
+++ src/features/shopSlice.js
@@ -1,7 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const LOCAL_KEY = "productsList";
+
 const initValue = {
-  shop_ar:[
+  shop_ar: localStorage[LOCAL_KEY] ? JSON.parse(localStorage[LOCAL_KEY]) : [
     {name:"Bamba", amount:3, id:1},
     {name:"Milk", amount:2, id:2},
   ]
@@ -13,13 +15,25 @@ const shopSlice = createSlice({
   reducers:{
     addProduct:(state,action) => {
       state.shop_ar.push(action.payload.item)
+      saveToLocal(state.shop_ar);
     },
     resetProducts:(state,action) => {
       // state.shop_ar = []
       state.shop_ar.splice(0)
+      saveToLocal(state.shop_ar);
+    },
+    deleteSingle:(state,action) => {
+      const delIndex = state.shop_ar.findIndex(item => 
+        item.id == action.payload.delId)
+      state.shop_ar.splice(delIndex,1);
+      saveToLocal(state.shop_ar);
     }
   }
 })
+// ישמור בלוקאל את המצב הנוכחי של הסטייט
+const saveToLocal = (state) => {
+  localStorage.setItem(LOCAL_KEY,JSON.stringify(state));
+}
 
-export const {addProduct,resetProducts} = shopSlice.actions;
+export const {addProduct,resetProducts,deleteSingle} = shopSlice.actions;
 export default shopSlice.reducer;
